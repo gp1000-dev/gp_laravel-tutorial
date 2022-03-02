48fc33d8433c94e32616e5ad58a744299e56eaa8
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-02 18:01:08 +0900

    【chapter03】コントローラークラス_TaskControllerの作成
    ターミナルで「php artisan make:controller TaskController」を実施してコントローラーを作成したら、このcommitにあるコードに上書きして更新してください。

---
+++ src/LaravelTaskList01/app/Http/Controllers/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Folder;
+
+class TaskController extends Controller
+{
+    // 入門3：ルーティングのテストコード
+    public function index()
+    {
+        return "Hello world";
+    }
+}
