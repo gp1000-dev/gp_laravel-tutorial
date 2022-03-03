507844300f3f98cbe44fa04a98b9353ca0045462
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-03 12:47:37 +0900

    【chapter03】コントローラー_TaskController.phpの更新
    TaskControllerのindexメソッドの内容を書き換えています。hello world を表示するものから、DBのfoldersテーブルのデータを取得して表示するものに変更がされています。

--- src/LaravelTaskList01/app/Http/Controllers/TaskController.php
+++ src/LaravelTaskList01/app/Http/Controllers/TaskController.php
@@ -7,9 +7,23 @@ use App\Models\Folder;
 
 class TaskController extends Controller
 {
-    // 入門3：ルーティングのテストコード
-    public function index()
+    // 入門3：コントローラーのテストコード
+    /**
+     *  Folderモデルの全てのデータをDBから取得するコントローラー
+     *
+     *  @param int $id
+     *  @return string
+     */
+    public function index(int $id)
     {
-        return "Hello world";
+        // Folderモデルの全てのデータをDBから取得する
+        $folders = Folder::all();
+        // indexテンプレートにFolderモデルの全てのデータを渡した結果を返す
+        // view('遷移先のbladeファイル名', [連想配列：渡したい変数についての情報]);
+        // 連想配列：['キー（テンプレート側で参照する際の変数名）' => '渡したい変数']
+        return view('tasks/index', [
+            'folders' => $folders,
+            "current_folder_id"=>$id
+        ]);
     }
 }
