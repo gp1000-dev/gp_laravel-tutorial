8a8a18ce4b9c383ba3b12e2fbfb0ac5e5ddaa181
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-04 10:53:35 +0900

    【chapter04】コントローラー_TaskController.phpの更新
    「ユーザーによって選択されたフォルダに紐づくタスクを取得する」と「ユーザーによって選択されたフォルダに紐づくタスクを取得する」を表示する機能を追加しています。

--- src/LaravelTaskList01/app/Http/Controllers/TaskController.php
+++ src/LaravelTaskList01/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 use App\Models\Folder;
 
 class TaskController extends Controller
@@ -17,13 +18,28 @@ class TaskController extends Controller
     public function index(int $id)
     {
         // Folderモデルの全てのデータをDBから取得する
+        // all()：全てのデータを取得する関数
         $folders = Folder::all();
+
+        // ユーザーによって選択されたフォルダを取得する
+        // find()：一行分のデータを取得する関数
+        $current_folder = Folder::find($id);
+
+        // ユーザーによって選択されたフォルダに紐づくタスクを取得する
+        // where(カラム名,カラムに対して比較する値)：SQLクエリにwhere節を追加する関数 ※こちらは省略形です
+        // get()：値を取得する関数（この場合はwhere関数で生成されたSQL文を発行して値を取得する）
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         // indexテンプレートにFolderモデルの全てのデータを渡した結果を返す
         // view('遷移先のbladeファイル名', [連想配列：渡したい変数についての情報]);
         // 連想配列：['キー（テンプレート側で参照する際の変数名）' => '渡したい変数']
         return view('tasks/index', [
+            // 'folders'に$foldersの値を代入する
             'folders' => $folders,
-            "current_folder_id"=>$id
+            // ユーザーによって選択された"current_folder_id"に$idの値を代入する
+            'current_folder_id' => $current_folder->id,
+            // 'tasks'に$tasksを代入する
+            'tasks' => $tasks
         ]);
     }
 }
