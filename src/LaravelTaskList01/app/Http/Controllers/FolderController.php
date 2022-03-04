95d86a6b961afd3429e3e71181e3dc228b126b78
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-04 13:14:19 +0900

    【chapter05】コントローラー_FolderController.phpの更新
    このcommitでは「Folderを新規作成してDBに書き込む処理」「リダイレクト」の機能を追加しています。

--- src/LaravelTaskList01/app/Http/Controllers/FolderController.php
+++ src/LaravelTaskList01/app/Http/Controllers/FolderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
+/* use App\Http\Requests\CreateFolder; */
 
 class FolderController extends Controller
 {
@@ -16,5 +18,32 @@ class FolderController extends Controller
         // createテンプレートにFolder新規作成画面のPathを渡した結果を返す
         // view('遷移先のbladeファイル名');
         return view('folders/create');
+    }
+
+    // 入門5：コントローラー のテストコード
+    /**
+     *  Folderを新規作成してDBに書き込む処理のコントローラー
+     *  @param Request $request （リクエストクラスの$request）
+     *  @return string
+     */
+    public function create(Request $request)
+    {
+        /* 新規作成のフォルダー名（タイトル）をDBに書き込む処理 */
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+        // インスタンスの状態をデータベースに書き込む
+        $folder->save();
+
+        /* 上記の処理実行後のリダイレクト */
+        // リダイレクト：別URLへの転送（リクエストされたURLとは別のURLに直ちに再リクエストさせます）
+        // route('遷移先のbladeファイル名', [連想配列：渡したい変数についての情報]);
+        // 連想配列：['キー（テンプレート側で参照する際の変数名）' => '渡したい変数']
+        // redirect():リダイレクトを実施する関数
+        // route():ルートPathを指定する関数
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
