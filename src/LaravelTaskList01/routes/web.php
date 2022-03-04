d08a7c2b0eb2753fd649e5597903299133c4e3a1
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-04 13:37:48 +0900

    【chapter05】テンプレート_index.blade.phpの更新
    このcommitではindexページの「フォルダを追加する」へ遷移できるように変更をしています。また、本来であればルーティングの時点で行うべき名前空間への「use App\Http\Controllers\FolderController;」の記述をしています。

--- src/LaravelTaskList01/routes/web.php
+++ src/LaravelTaskList01/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 
 /*
 |--------------------------------------------------------------------------
