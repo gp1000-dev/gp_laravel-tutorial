24cc7c087f6514ce4737e5f91e92072c9dbe81b2
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-04 14:46:27 +0900

    【chapter05】FormRequest クラス_CreateFolder.phpの作成
    入力値バリデーションのために CreateFolder.php を作成しています。

---
+++ src/LaravelTaskList01/app/Http/Requests/CreateFolder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFolder extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * リクエストの内容に基づいた権限チェックを行う関数
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // return false;
+        // 返り値にtrueを指定する（リクエストを受け付ける）
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * バリデーションルールを定義する関数
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // タイトルの入力欄を入力必須に定義する
+            // 'title' => 'required',
+
+            // タイトルの入力欄を入力必須の最大文字数20文字に定義する
+            'title' => 'required|max:20',
+        ];
+    }
+}
