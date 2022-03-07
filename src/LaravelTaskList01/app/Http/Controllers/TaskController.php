8f19101e877603f811bed18aea78c628cad0f5c3
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-07 12:32:59 +0900

    【chapter06】コントローラー_TaskController.phpの更新
    タスクを新規作成してDBに書き込むためのコントローラーを追加しています。

--- src/LaravelTaskList01/app/Http/Controllers/TaskController.php
+++ src/LaravelTaskList01/app/Http/Controllers/TaskController.php
@@ -5,10 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Models\Folder;
-
+use App\Http\Requests\CreateTask;
 class TaskController extends Controller
 {
-    // 入門3：コントローラーのテストコード
     /**
      *  Folderモデルの全てのデータをDBから取得するコントローラー
      *  GET /folders/{id}/tasks
@@ -42,7 +41,6 @@ class TaskController extends Controller
         ]);
     }
 
-    // 入門6：コントローラー のテストコード
     /**
      *  フォルダIDを取得するためのコントローラー
      *  GET /folders/{id}/tasks/create
@@ -56,6 +54,40 @@ class TaskController extends Controller
         // 連想配列：['キー（テンプレート側で参照する際の変数名）' => '渡したい変数']
         return view('tasks/create', [
             'folder_id' => $id
+        ]);
+    }
+
+    /**
+     *  タスクを新規作成してDBに書き込む処理のコントローラー
+     *
+     *  @param int $id
+     *  @param CreateTask $request
+     *  @return string
+     */
+    public function create(int $id, CreateTask $request)
+    {
+        // ユーザーによって選択されたフォルダを取得する
+        // find()：一行分のデータを取得する関数
+        $current_folder = Folder::find($id);
+
+        /* 新規作成のタスク（タイトル）をDBに書き込む処理 */
+        // タスクモデルのインスタンスを作成する
+        $task = new Task();
+        // タイトルに入力値を代入する
+        $task->title = $request->title;
+        // 期限に入力値を代入する
+        $task->due_date = $request->due_date;
+        // $current_folderに紐づくタスクを生成する（インスタンスの状態をデータベースに書き込む）
+        $current_folder->tasks()->save($task);
+
+        /* 上記の処理実行後のリダイレクト */
+        // リダイレクト：別URLへの転送（リクエストされたURLとは別のURLに直ちに再リクエストさせます）
+        // route('遷移先のbladeファイル名', [連想配列：渡したい変数についての情報]);
+        // 連想配列：['キー（テンプレート側で参照する際の変数名）' => '渡したい変数']
+        // redirect():リダイレクトを実施する関数
+        // route():ルートPathを指定する関数
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
