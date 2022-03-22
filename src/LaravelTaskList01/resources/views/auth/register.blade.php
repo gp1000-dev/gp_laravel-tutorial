6476762e3ba5ee2c1d33f4a2a439c0e9a6fb4173
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-22 17:36:07 +0900

    【chapter08】認証機能_register.blade.phpの更新
    Laravel入門8.5の資料③-認証機能5を実施しています。ここでは会員登録画面のテンプレートを書き換えています。

--- src/LaravelTaskList01/resources/views/auth/register.blade.php
+++ src/LaravelTaskList01/resources/views/auth/register.blade.php
@@ -1,76 +1,68 @@
-@extends('layouts.app')
+<!--
+*   extends：親ビューを継承する（読み込む）
+*   親ビュー名：layout を指定
+-->
+@extends('layout')
 
+<!--
+*   section：子ビューにsectionでデータを定義する
+*   セクション名：content を指定
+*   用途：会員登録画面を表示する
+-->
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+    <div class="row">
+        <div class="col col-md-offset-3 col-md-6">
+            <nav class="panel panel-default">
+                <div class="panel-heading">会員登録</div>
+                <div class="panel-body">
+                    <!-- エラーメッセージを表示する -->
+                    <!-- エラーがある場合はIF文の処理を実行する -->
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                <!-- エラーメッセージをループで全て列挙して表示する -->
+                                @foreach($errors->all() as $message)
+                                    <li>{{ $message }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <!-- Auth::routes()に定義されたルートを指定（php artisan route:list で参照できます） -->
+                    <form action="{{ route('register') }}" method="POST">
+                        <!-- セキュリティ対策：@csrf は、CSRFトークンを含んだ input 要素を出力します -->
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="email">メールアドレス</label>
+                            <!--入力値を復元する
+                            *   old()：セッション値を取得する関数（この場合は入力されたタイトルをセッションとして扱う）
+                            -->
+                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="name">ユーザー名</label>
+                            <!--入力値を復元する
+                            *   old()：セッション値を取得する関数（この場合は入力されたタイトルをセッションとして扱う）
+                            -->
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" />
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="password">パスワード</label>
+                            <input type="password" class="form-control" id="password" name="password">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="form-group">
+                            <label for="password-confirm">パスワード（確認）</label>
+                            <!-- パスワード一致確認機能（ confirmed ルール）
+                            *    name="目名（password）_confirmation"で表現されて入力値が一致するか検証しています。
+                            -->
+                            <input type="password" class="form-control" id="password-confirm" name="password_confirmation">
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                        <div class="text-right">
+                            <button type="submit" class="btn btn-primary">送信</button>
                         </div>
                     </form>
                 </div>
-            </div>
+            </nav>
         </div>
     </div>
 </div>
