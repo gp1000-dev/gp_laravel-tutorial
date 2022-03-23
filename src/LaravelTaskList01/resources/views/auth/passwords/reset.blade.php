1ea9c8c517a36d4f8d2a3864ded6c5c1b9ce8201
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-23 17:22:08 +0900

    【chapter09】メール認証_パスワード再設定ページの更新
    Laravel入門9の資料⑤-メール認証3を実施しています。ここでは、パスワード再設定ページを編集しています。

--- src/LaravelTaskList01/resources/views/auth/passwords/reset.blade.php
+++ src/LaravelTaskList01/resources/views/auth/passwords/reset.blade.php
@@ -1,64 +1,45 @@
-@extends('layouts.app')
+<!--
+*   extends()：親ビューを継承する（読み込む）
+*   親ビュー名：layout を指定
+-->
+@extends('layout')
 
+<!--
+*   section()：子ビューにsectionでデータを定義する
+*   セクション名：content を指定
+*   用途：パスワード再設定ページを表示する
+-->
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
+    <div class="row">
+        <div class="col col-md-offset-3 col-md-6">
+            <nav class="panel panel-default">
+                <div class="panel-heading">パスワード再発行</div>
+                <div class="panel-body">
+                    <form action="{{ route('password.update') }}" method="POST">
+                        <!-- セキュリティ対策：@csrf は、CSRFトークンを含んだ input 要素を出力します -->
                         @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="email">メールアドレス</label>
+                            <input type="text" class="form-control" id="email" name="email" />
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="password">新しいパスワード</label>
+                            <input type="password" class="form-control" id="password" name="password" />
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="form-group">
+                            <label for="password-confirm">新しいパスワード（確認）</label>
+                            <!-- パスワード一致確認機能（ confirmed ルール）
+                            *    name="目名（password）_confirmation"で表現されて入力値が一致するか検証しています。
+                            -->
+                            <input type="password" class="form-control" id="password-confirm" name="password_confirmation" />
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="text-right">
+                        <button type="submit" class="btn btn-primary">送信</button>
                         </div>
                     </form>
                 </div>
-            </div>
+            </nav>
         </div>
     </div>
 </div>
