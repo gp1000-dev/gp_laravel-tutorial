34115e49fc251720c8232bbbab6fb52c53b987f8
eguchi2404 <68371224+eguchi2404@users.noreply.github.com>
2022-03-23 12:38:00 +0900

    【chapter08】認証機能_login.blade.phpの更新
    Laravel入門8.5の資料③-認証機能8を実施しています。ここではログイン画面のテンプレートを書き換えています。

--- src/LaravelTaskList01/resources/views/auth/login.blade.php
+++ src/LaravelTaskList01/resources/views/auth/login.blade.php
@@ -1,71 +1,55 @@
-@extends('layouts.app')
+<!--
+*   extends：親ビューを継承する（読み込む）
+*   親ビュー名：layout を指定
+-->
+@extends('layout')
 
+<!--
+*   section：子ビューにsectionでデータを定義する
+*   セクション名：content を指定
+*   用途：ログイン画面を表示する
+-->
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+    <div class="row">
+        <div class="col col-md-offset-3 col-md-6">
+            <nav class="panel panel-default">
+                <div class="panel-heading">ログイン</div>
+                <div class="panel-body">
+                    <!-- エラーメッセージを表示する -->
+                    <!-- エラーがある場合はIF文の処理を実行する -->
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                        <ul>
+                            <!-- エラーメッセージをループで全て列挙して表示する -->
+                            @foreach($errors->all() as $message)
+                                <li>{{ $message }}</li>
+                            @endforeach
+                        </ul>
+                        </div>
+                    @endif
+                    <form action="{{ route('login') }}" method="POST">
+                        <!-- セキュリティ対策：@csrf は、CSRFトークンを含んだ input 要素を出力します -->
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="email">メールアドレス</label>
+                            <!--入力値を復元する
+                            *   old()：セッション値を取得する関数（この場合は入力されたタイトルをセッションとして扱う）
+                            -->
+                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label for="password">パスワード</label>
+                            <input type="password" class="form-control" id="password" name="password" />
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                            <div class="text-right">
+                            <button type="submit" class="btn btn-primary">送信</button>
                         </div>
                     </form>
                 </div>
+            </nav>
+            <div class="text-center">
+                <a href="{{ route('password.request') }}">パスワードの変更はこちらから</a>
             </div>
         </div>
     </div>
