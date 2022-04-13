7903feebd059980631f88a182df3135c905cdd63
eguchi-gp1000 <eguchi@gp1000.jp>
2022-04-13 15:36:51 +0900

    【chapter10】AuthServiceProvider.phpの更新
    Laravel入門10の資料①-エラーハンドリング15を実施しています。ここでは、フィルダーポリシーとフォルダーモデルを紐づけています。

--- src/LaravelTaskList01/app/Providers/AuthServiceProvider.php
+++ src/LaravelTaskList01/app/Providers/AuthServiceProvider.php
@@ -4,22 +4,26 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\Folder;
+use App\Policies\FolderPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
+     *  The policy mappings for the application.
+     *  機能：ポリシーを AuthServiceProvider に登録する
+     *  用途：ポリシーとモデルを紐づける
+     *  @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // フィルダーポリシーとフォルダーモデルを紐づける
+        Folder::class => FolderPolicy::class,
     ];
 
     /**
-     * Register any authentication / authorization services.
+     *  Register any authentication / authorization services.
      *
-     * @return void
+     *  @return void
      */
     public function boot()
     {
